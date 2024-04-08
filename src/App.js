c6ce3cfbe760613f375c738e21da1a32b1fbfd65
smokyshiv <chnarsappa06@gmail.com>
2024-04-08 07:34:37 +0000

    added exact to the home route

--- src/App.js
+++ src/App.js
@@ -148,7 +148,7 @@ const App = () => {
 
   const renderSuccessView = () => (
     <>
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
       <Route exact path="/cart" component={CartRoute} />
     </>
   )
